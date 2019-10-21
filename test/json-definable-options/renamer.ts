b0ca0fe32c172f6c741bda23a901cd2c14abc5bc
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-10-21 15:47:37 +0900

    ✅ Add tests for defining plugin options and renamer option in external script files

---
+++ test/json-definable-options/renamer.ts
@@ -0,0 +1,31 @@
+import test from 'ava';
+import Metalsmith from 'metalsmith';
+
+import { normalizeOptions } from '../../src/options';
+import fixtures from './fixtures';
+
+test.before(() => {
+    process.chdir(fixtures());
+});
+
+test('should import a script file that defines renamer option', async t => {
+    const options = await normalizeOptions({}, Metalsmith(__dirname), {
+        renamer: './renamer-option',
+    });
+    const expectedOptions = await normalizeOptions({}, Metalsmith(__dirname), {
+        renamer: require(fixtures('./renamer-option')),
+    });
+    t.deepEqual(options, expectedOptions);
+});
+
+test('should fail import a non exist script file that defines renamer option', async t => {
+    await t.throwsAsync(
+        normalizeOptions({}, Metalsmith(__dirname), {
+            renamer: './non-exist',
+        }),
+        {
+            instanceOf: Error,
+            message: `Loading renamer failed: Cannot find module './non-exist'`,
+        },
+    );
+});
